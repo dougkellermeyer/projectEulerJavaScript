a43c3e2f09bfdc7f32d1dbd61ee16b63bcac9f54
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-01-21 17:07:20 -0500

    it works...kinda

--- js/problem3.js
+++ js/problem3.js
@@ -6,20 +6,34 @@
 
 //define a prime number
 
-var randomNumbers = [
-    1,2,3,4,5,6,7,9,10
+var primeNumbers = [
+    2,3,5,7,11,13,17,19, 23,
+    29,31,37,41,43,47,53,59,61,
+    67,71,73,79,83,89,97, 101,
+    103,107,109,113,127,131,137,
+    139,149,151,157,163,167,173,
+    179,181,191,193,197,199,
+    211,223,227,229,233,239,241,
+    251,257,263,269,271,277, 281,
+    283,293,307,311,313
 ]
 
 var primeFactors = [];
-//should be 2,3,5,7
 
-function findPrimeFactors (num, divisor) {
-    if(num % divisor === 0){
-        primeFactors.push(divisor);
+primeNumbers.forEach(function(primeNum){
+    n = 13195; 
+    if (n % primeNum == 0) {
+        primeFactors.push(primeNum);
     }
-}
+});
 
-findPrimeFactors(10);
+var maxPrimeFactor = primeFactors.reduce(function(a,b){
+    return Math.max(a,b);
+});
+
    
-
 console.log(primeFactors);
+console.log(maxPrimeFactor);
+
+
+
